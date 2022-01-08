cf8264b45dd118737f2421969bf58613d87a5509
ewaspioch <spiochewa@gmail.com>
2022-01-08 08:33:20 +0100

    Add logout, changed UserProfile, add location detection

--- app/src/main/java/com/example/learnwordsapp/UserProfile.java
+++ app/src/main/java/com/example/learnwordsapp/UserProfile.java
@@ -1,26 +1,101 @@
 package com.example.learnwordsapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+import static androidx.constraintlayout.motion.widget.Debug.getLocation;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
 import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.provider.Settings;
 import android.service.notification.StatusBarNotification;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.TextView;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class UserProfile extends AppCompatActivity {
-    Switch notif_switch;
+    private static final int REQUEST_CODE = 0101;
+    private Switch notif_switch;
+    private Switch sound_switch;
+    private TextView location_text;
+    private TextView language_text;
+    private Button button_edit_avatar;
+    private Button button_edit_profile;
+    private Button button_logout;
+
+
+    private FusedLocationProviderClient fusedLocationClient;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
-        Switch notif_switch = (Switch) findViewById(R.id.switch_notification);
+
+        notif_switch = (Switch) findViewById(R.id.switch_notification);
+        sound_switch = (Switch) findViewById(R.id.switch_sound);
+        location_text = findViewById(R.id.location_text);
+        language_text = findViewById(R.id.language_text);
+        button_edit_avatar = findViewById(R.id.button_editavatar);
+        button_edit_profile = findViewById(R.id.button_editprofile);
+        button_logout = findViewById(R.id.button_logout);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        permLocateUser();
+
+        button_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(UserProfile.this, LoginActivity.class));
+                finish();
+            }
+        });
+
+        button_edit_avatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        button_edit_profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        sound_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+
+            }
+        });
 
         notif_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
@@ -36,11 +111,42 @@ public class UserProfile extends AppCompatActivity {
                     intent.putExtra("android.provider.extra.APP_PACKAGE", getPackageName());
 
                     startActivity(intent);
-
                 }
             }
         });
 
+    }
+
+    private void permLocateUser() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_CODE);
+        }
+        locateUser();
+    }
+
+    private void locateUser(){
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            fusedLocationClient.getLastLocation().addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                @Override
+                public void onSuccess(Location location) {
+                    if (location != null) {
+                        try {
+                            Geocoder geocoder = new Geocoder(UserProfile.this, Locale.getDefault());
+                            List<Address> addressList = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+                            location_text.setText("" + addressList.get(0).getCountryName());
+
+                        } catch (IOException e) {
+                            location_text.setText("No Location");
+                            e.printStackTrace();
+                        }
+                    }
+                    else{
+                        location_text.setText("No Location");
+                    }
+                }
+            });
+        }
     }
 
 }
