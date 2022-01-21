8148dc53e4ce6fce908e456c2c6a3e06c12f319c
Karolina Jakubowska <kuki2000j@icloud.com>
2022-01-22 00:10:09 +0100

    Save and display profile photo

--- app/src/main/java/com/example/learnwordsapp/UserProfileFragment.java
+++ app/src/main/java/com/example/learnwordsapp/UserProfileFragment.java
@@ -2,10 +2,16 @@ package com.example.learnwordsapp;
 
 import static android.content.Context.MODE_PRIVATE;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
@@ -18,12 +24,16 @@ import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -32,20 +42,29 @@ import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
 public class UserProfileFragment extends Fragment {
     private static final int REQUEST_CODE = 0101;
-    public static final int CAMERA_PERM_CODE = 101;
-    public static final int CAMERA_REQUEST_CODE = 102;
 
     View root;
     Switch notif_switch;
@@ -58,11 +77,73 @@ public class UserProfileFragment extends Fragment {
     Button statistics_button;
     Button logout_button;
     ImageView avatar_image;
+    Bitmap profileImage;
+    Boolean cameraGranted = false;
+    String currentPhotoPath;
+    String currentPhotoPath_s;
+    Uri outputFileUri = null;
+    Uri outputFileUri_s = null;
+    FirebaseAuth mAuth = FirebaseAuth.getInstance();
 
     private MediaPlayer musicP;
     private Switch darkMode_switch;
 
     private FusedLocationProviderClient fusedLocationClient;
+
+    private ActivityResultLauncher<String> mCameraPermissionResult = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            new ActivityResultCallback<Boolean>() {
+                @Override
+                public void onActivityResult(Boolean result) {
+                    String TAG = "CAMERA_PERM_REQ";
+                    if(result) {
+                        Log.e(TAG, "onActivityResult: PERMISSION GRANTED");
+                    } else {
+                        Log.e(TAG, "onActivityResult: PERMISSION DENIED");
+                    }
+                }
+            });
+
+    ActivityResultLauncher<Intent> cameraResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        try {
+                            Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), outputFileUri);
+
+                            Bitmap profileImage = Bitmap.createScaledBitmap(bitmap,  120 ,120, true);
+                            saveBitmapFile(profileImage, currentPhotoPath_s);
+                            //profileImage = RotateBitmap(profileImage,270);
+                            uploadImage(currentPhotoPath_s);
+                            avatar_image.setImageBitmap(profileImage);
+
+                        } catch(Exception e){
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
+            });
+
+    private ActivityResultLauncher<String> storagePermissionResult = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            new ActivityResultCallback<Boolean>() {
+                @Override
+                public void onActivityResult(Boolean result) {
+                    String TAG = "STORAE_PERM_REQ";
+                    if(result) {
+                        Log.e(TAG, "onActivityResult: PERMISSION GRANTED");
+
+                    } else {
+
+                        Log.e(TAG, "onActivityResult: PERMISSION DENIED");
+                    }
+                }
+            });
+
+
 
     public UserProfileFragment() {
         // Required empty public constructor
@@ -85,7 +166,7 @@ public class UserProfileFragment extends Fragment {
         root = inflater.inflate(R.layout.fragment_user_profile, container, false);
 
         notif_switch = root.findViewById(R.id.switch_notification);
-        sound_switch = root.findViewById(R.id.switch_sound);
+       // sound_switch = root.findViewById(R.id.switch_sound);
         location_text = root.findViewById(R.id.location_text);
         edit_profile_button = root.findViewById(R.id.button_editprofile);
         edit_avatar_button = root.findViewById(R.id.button_editavatar);
@@ -95,21 +176,35 @@ public class UserProfileFragment extends Fragment {
         change_language_button = root.findViewById(R.id.button_changeMyLang);
         darkMode_switch = root.findViewById(R.id.switch_mode);
 
-        //ActionBar actionBar = getSupportActionBar();
-        //actionBar.setTitle(getResources().getString(string.app_name));
-
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(container.getContext());
 
         permLocateUser();
 
+        dowloadProfileImage();
+
         edit_avatar_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //askCameraPermissions();
-                Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(camera, CAMERA_REQUEST_CODE);
+
+                if(ContextCompat.checkSelfPermission(root.getContext(), Manifest.permission.CAMERA)
+                        !=PackageManager.PERMISSION_GRANTED) {
+                    mCameraPermissionResult.launch(Manifest.permission.CAMERA);
+                }
+                if(ContextCompat.checkSelfPermission(root.getContext(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        !=PackageManager.PERMISSION_GRANTED){
+                    storagePermissionResult.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                }
+
+                try {
+                    saveImage();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
+
         });
+
 
         edit_profile_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -192,24 +287,124 @@ public class UserProfileFragment extends Fragment {
 
         return root;
     }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == CAMERA_REQUEST_CODE) {
-            Bitmap image = (Bitmap) data.getExtras().get("data");
-            avatar_image.setImageBitmap(image);
-            //saveAvatar(avatar);
-        }
+    private void uploadImage(@NonNull String path) {
+        String UID = mAuth.getCurrentUser().getUid();
+        FirebaseDatabase storage = FirebaseDatabase.getInstance();
+        DatabaseReference storageRef = storage.getReference("/Users");
+        storageRef
+                .child(UID)
+                .child("PhotoURI")
+                .setValue(path)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getContext(), "File '" + path + "' is your profile image", Toast.LENGTH_LONG).show();
+                        } else {
+                            Toast.makeText(getContext(), "Setting your profile image failed!", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 
-    private void askCameraPermissions() {
-        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
-        } else {
+    private void dowloadProfileImage() {
+        String UID = mAuth.getCurrentUser().getUid();
+        FirebaseDatabase storage = FirebaseDatabase.getInstance();
+        Task<DataSnapshot> storageRef = storage.getReference("/Users")
+                .child(UID)
+                .child("PhotoURI")
+                .get()
+                .addOnSuccessListener(new OnSuccessListener<DataSnapshot>() {
+                    @Override
+                    public void onSuccess(DataSnapshot dataSnapshot) {
+                        Object ori = dataSnapshot.getValue();
+                        if(ori==null)
+                            return;
 
-        }
+                        currentPhotoPath_s = ori.toString();
+                        outputFileUri_s = Uri.fromFile(new File(currentPhotoPath_s));
+
+                        try {
+                            Bitmap profileImage = MediaStore.Images.Media.getBitmap(getContext().getContentResolver(), outputFileUri_s);
+                            avatar_image.setImageBitmap(profileImage);
+
+                        } catch(Exception e){
+                            e.printStackTrace();
+                        }
+                    }
+                });
+
+
     }
+
+    public static Bitmap RotateBitmap(Bitmap source, float angle) {
+        Matrix matrix = new Matrix();
+        matrix.postRotate(angle);
+        return Bitmap.createBitmap(source, 0, 0, source.getWidth(), source.getHeight(), matrix, true);
+    }
+
+    private void saveImage() throws IOException {
+
+        File image = createImageFile();
+        outputFileUri = FileProvider.getUriForFile(getContext(),
+                "com.example.learnwordsapp.fileprovider",
+                image);
+
+            Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            camera.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+            camera.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            cameraResultLauncher.launch(camera);
+
+    }
+
+    private File createImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "IMG_LW_" + timeStamp + "_";
+        String imageFileName2 = "IMG_LW_" + timeStamp + "_S";
+        File storageDir = getContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File image1 = File.createTempFile(
+                imageFileName,
+                ".jpg",
+                storageDir
+        );
+
+
+        currentPhotoPath = image1.getAbsolutePath();
+        outputFileUri = Uri.fromFile(image1);
+
+
+        File image2 = File.createTempFile(
+                imageFileName2,
+                ".jpg",
+                storageDir
+        );
+        currentPhotoPath_s = image2.getAbsolutePath();
+        outputFileUri_s = Uri.fromFile(image2);
+
+        return image1;
+    }
+
+    private void saveBitmapFile(@NonNull Bitmap bmp, @NonNull String f){
+        String file_path = f;
+        File file = new File(file_path);
+        FileOutputStream fOut;
+        try {
+            fOut = new FileOutputStream(file);
+            if(fOut != null){
+                bmp.compress(Bitmap.CompressFormat.JPEG, 95, fOut);
+                fOut.flush();
+                fOut.close();
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 
     private void permLocateUser() {
         if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
